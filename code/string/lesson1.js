b706fb172efa5ac44a74f2b41361d700fe0d11ff
wyyyyyy <301059120@qq.com>
2019-05-12 22:09:50 +0800

    2019-5-12

---
+++ code/string/lesson1.js
@@ -0,0 +1,46 @@
+// export default str => {
+//   let arr = str.split(' ')
+//   let result = arr.map(item => {
+//     return item
+//       .split('')
+//       .reverse('')
+//       .join('')
+//   })
+//   return result.join(' ')
+// }
+
+// export default str => {
+//   return str
+//     .split(' ')
+//     .map(item => {
+//       return item
+//         .split('')
+//         .reverse('')
+//         .join('')
+//     })
+//     .join(' ')
+// }
+
+// export default str => {
+//   return str
+//     .split(/\s/g)
+//     .map(item => {
+//       return item
+//         .split('')
+//         .reverse('')
+//         .join('')
+//     })
+//     .join(' ')
+// }
+
+export default str => {
+  return str
+    .match(/[\w']+/g)
+    .map(item => {
+      return item
+        .split('')
+        .reverse('')
+        .join('')
+    })
+    .join(' ')
+}
---
+++ test/string/lesson1.test.js
@@ -0,0 +1,7 @@
+import revertWorld from '../../code/string/lesson1'
+
+test("Let's take LeetCode contest", () => {
+  expect(revertWorld("Let's take LeetCode contest")).toBe(
+    "s'teL ekat edoCteeL tsetnoc"
+  )
+})
